3333427ba9313b8b3e53a3a48093399b531e6587
minju26 <mjalswn26@gmail.com>
2024-08-02 16:06:39 +0900

    fix(AuthController): 클라이언트 url 저장, redirection 하도록 수정

--- src/main/java/gift/controller/AuthController.java
+++ src/main/java/gift/controller/AuthController.java
@@ -7,8 +7,6 @@ import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
